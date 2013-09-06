aca0f7ac9bd6f3c8c978310e20259e3a46c33a1d
Shusui MOYATANI <syusui.s@gmail.com>
2013-09-07 01:37:45 +0900

    ブックマークレットの導入がわかりやすいように、UIを追加した。

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+function init(){
+	var bookmarklet = document.getElementById('bookmarklet');
+
+	bookmarklet.onmouseover = function(){
+		var elem = document.getElementById('drag_to_bookmarkbar');
+		elem.style.display = 'block';
+	};
+	bookmarklet.onmouseout  = function(){
+		var elem = document.getElementById('drag_to_bookmarkbar');
+		elem.style.display = 'none';
+	}
+}
